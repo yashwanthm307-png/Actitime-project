3cef7351ebfc71a1915601511785727df1bba2c4
hari984422 <hari9844225181@gmail.com>
2025-10-27 12:48:51 +0530

    Declared one element in UserListPage

--- Actitime/src/com/actitime/pom/UserListPage.java
+++ Actitime/src/com/actitime/pom/UserListPage.java
@@ -1,8 +1,12 @@
 package com.actitime.pom;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class UserListPage {
+	@FindBy(id="username")
+	private WebElement unTbx;
 public UserListPage(WebDriver driver){
   PageFactory.initElements(driver,this);
 }
