d77c2cea5d5ff588d10fb952682f74aa3f9f8435
yashwanthm307-png <yashwanthm307@gmail.com>
2025-10-27 12:45:52 +0530

    Initialize UserListPage with WebDriver

--- Actitime/src/com/actitime/pom/UserListPage.java
+++ Actitime/src/com/actitime/pom/UserListPage.java
@@ -1,5 +1,10 @@
 package com.actitime.pom;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class UserListPage {
-
+public UserListPage(WebDriver driver){
+  PageFactory.initElements(driver,this);
+}
+  
 }
